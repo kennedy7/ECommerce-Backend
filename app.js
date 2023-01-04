59127ad905221480adf87ec420c063aa3b3273e1
unknown <kennedyhillary6@gmail.com>
2023-01-04 18:41:18 +0100

    mongoose

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require ("express")
 const cors = require ("cors")
 const app = express()
 const PORT = process.env.PORT || 5000 
+const products = require("./products")
 
 app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res)=>{
     res.send("Welcome to the backend homepage")
+})
+app.get('/products', (req, res)=>{
+    res.send(products)
 })
 app.listen(PORT, ()=>{
     console.log(`Ecommerce backend running on port ${PORT} `)
---
+++ products.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    id: 1,
+    name: "Iphone 13",
+    desc: " 7-inch HD display",
+    price: 500,
+    image:
+      "https://res.cloudinary.com/dqxhz6sro/image/upload/v1672243205/OnlineShop/iphone12pro_lnfnyo.jpg",
+  },
+  {
+    id: 2,
+    name: "Samsung phone",
+    desc: "7-inch display",
+    price: 100,
+    image:
+      "https://res.cloudinary.com/dqxhz6sro/image/upload/v1672243205/OnlineShop/galaxyS_aq6fmf.png",
+  },
+];
+module.exports = products;
