63595da6141e8a037191805f290961632f33ac6a
unknown <kennedyhillary6@gmail.com>
2023-01-04 19:16:23 +0100

    User Model

--- models/user.js
+++ models/user.js
@@ -4,5 +4,23 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 200,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 1000,
+    unique: true,
   },
 });
+
+module.exports = mongoose.model("User", userSchema);
