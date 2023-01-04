f4f10088c3785c861ce3dee007df621438f22b41
unknown <kennedyhillary6@gmail.com>
2023-01-04 18:49:01 +0100

    mongoose

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const express = require ("express")
-const cors = require ("cors")
-const app = express()
-const PORT = process.env.PORT || 5000 
-const products = require("./products")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.use(express.json())
-app.use(cors())
+const app = express();
+const PORT = process.env.PORT || 5000;
+const products = require("./products");
 
-app.get('/', (req, res)=>{
-    res.send("Welcome to the backend homepage")
-})
-app.get('/products', (req, res)=>{
-    res.send(products)
-})
-app.listen(PORT, ()=>{
-    console.log(`Ecommerce backend running on port ${PORT} `)
-})
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the backend homepage");
+});
+app.get("/products", (req, res) => {
+  res.send(products);
+});
+app.listen(PORT, () => {
+  console.log(`Ecommerce backend running on port ${PORT} `);
+});
+
+const url = process.env.DB_URL;
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("database connected successfully"));
