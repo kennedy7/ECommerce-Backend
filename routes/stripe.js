9e0ac73ce7bdde8d77c97830020873603f782fce
unknown <kennedyhillary6@gmail.com>
2023-01-11 07:12:50 +0100

    stripe

--- routes/stripe.js
+++ routes/stripe.js
@@ -22,6 +22,21 @@ StripeRouter.post("/api/stripe/create-checkout-session", async (req, res) => {
     };
   });
   const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_address_collection: { allowed_countries: ["US", "CA"] },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: { amount: 0, currency: "usd" },
+          display_name: "Free shipping",
+          delivery_estimate: {
+            minimum: { unit: "business_day", value: 5 },
+            maximum: { unit: "business_day", value: 7 },
+          },
+        },
+      },
+    ],
     line_items,
     mode: "payment",
     success_url: `${process.env.CLIENT_URL}/checkout-success`,
