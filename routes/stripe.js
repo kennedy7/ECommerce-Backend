644f81559c05ab603624620ac406d870d1bcd668
unknown <kennedyhillary6@gmail.com>
2023-01-11 07:00:15 +0100

    stripe

--- routes/stripe.js
+++ routes/stripe.js
@@ -4,19 +4,25 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 StripeRouter.post("/api/stripe/create-checkout-session", async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "T-shirt",
+  const line_items = req.body.cartItems.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          description: item.desc,
+          metadata: {
+            id: item.id,
           },
-          unit_amount: 200,
         },
-        quantity: 1,
+        unit_amount: item.price * 100,
       },
-    ],
+      quantity: item.cartQuantity,
+    };
+  });
+  const session = await stripe.checkout.sessions.create({
+    line_items,
     mode: "payment",
     success_url: `${process.env.CLIENT_URL}/checkout-success`,
     cancel_url: `${process.env.CLIENT_URL}/cart`,
