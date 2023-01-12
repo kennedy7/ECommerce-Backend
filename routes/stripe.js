48e85d84f0048e73fad9bde29d5557939d8542fb
unknown <kennedyhillary6@gmail.com>
2023-01-12 06:12:22 +0100

    stripe webHook

--- routes/stripe.js
+++ routes/stripe.js
@@ -81,15 +81,6 @@ StripeRouter.post(
     }
 
     // Handle the event
-    switch (event.type) {
-      case "payment_intent.succeeded":
-        const paymentIntent = event.data.object;
-        // Then define and call a function to handle the event payment_intent.succeeded
-        break;
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
 
     // Return a 200 response to acknowledge receipt of the event
     response.send();
