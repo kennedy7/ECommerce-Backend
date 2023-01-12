77a867618241ed4dfa04937335b96c0ce9846770
unknown <kennedyhillary6@gmail.com>
2023-01-12 06:59:51 +0100

    stripe webHook

--- routes/stripe.js
+++ routes/stripe.js
@@ -76,8 +76,8 @@ let endpointSecret;
 StripeRouter.post(
   "/api/stripe/webhook",
   express.raw({ type: "application/json" }),
-  (request, response) => {
-    const sig = request.headers["stripe-signature"];
+  (req, res) => {
+    const sig = req.headers["stripe-signature"];
 
     let data;
     let eventType;
@@ -87,15 +87,11 @@ StripeRouter.post(
       let event;
 
       try {
-        event = stripe.webhooks.constructEvent(
-          request.body,
-          sig,
-          endpointSecret
-        );
+        event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
         console.log("Webhook verified");
       } catch (err) {
         console.log(`Webhook Error: ${err.message}`);
-        response.status(400).send(`Webhook Error: ${err.message}`);
+        res.status(400).send(`Webhook Error: ${err.message}`);
         return;
       }
       data = req.body.data.object;
@@ -107,9 +103,13 @@ StripeRouter.post(
 
     // Handle the event
     if (eventType === "checkout.session.completed") {
-      stripe.customers.retrieve(data.customer).then((customer) => {
-        console.log(customer);
-      });
+      stripe.customers
+        .retrieve(data.customer)
+        .then((customer) => {
+          console.log(customer);
+          console.log("data", data);
+        })
+        .catch((err) => console.log(err.message));
     }
     // Return a 200 response to acknowledge receipt of the event
     response.send().end();
