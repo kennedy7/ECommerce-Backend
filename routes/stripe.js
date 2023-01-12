ac2aa1e3e68d241082668405a2ee9a8bb7a049d1
unknown <kennedyhillary6@gmail.com>
2023-01-12 06:10:20 +0100

    stripe webHook

--- routes/stripe.js
+++ routes/stripe.js
@@ -61,4 +61,39 @@ StripeRouter.post("/api/stripe/create-checkout-session", async (req, res) => {
   res.send({ url: session.url });
 });
 
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret =
+  "whsec_42bfc8aad225197927d1f6adce4861dd4fbaf7dc37a96a503fb5db0332418e64";
+
+StripeRouter.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
+
 module.exports = StripeRouter;
