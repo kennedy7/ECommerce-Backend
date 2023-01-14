8e0ee9b18c5603e596468fe569ad571a697f215c
unknown <kennedyhillary6@gmail.com>
2023-01-14 06:10:15 +0100

    orderSchema

--- models/order.js
+++ models/order.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
-  },
-  products: [
-    {
-      id: { type: String },
-      name: { type: String },
-      brand: { type: String },
-      desc: { type: String },
-      price: { type: String },
-      image: { type: String },
-      cartQuantity: { type: Number },
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
     },
-  ],
-  subtotal: { type: Number, required: true },
-  total: { type: Number, required: true },
-  shipping: { type: Object, required: true },
-  deliveryStatus: { type: Object, default: "pending" },
-  paymentStatus: { type: Object, required: true },
-});
+    products: [
+      {
+        id: { type: String },
+        name: { type: String },
+        brand: { type: String },
+        desc: { type: String },
+        price: { type: String },
+        image: { type: String },
+        cartQuantity: { type: Number },
+      },
+    ],
+    subtotal: { type: Number, required: true },
+    total: { type: Number, required: true },
+    shipping: { type: Object, required: true },
+    deliveryStatus: { type: Object, default: "pending" },
+    paymentStatus: { type: Object, required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Order", orderSchema);
