c27bd32ef31466bd011d896c5c9c5f7cdebd85a3
unknown <kennedyhillary6@gmail.com>
2023-01-15 07:07:51 +0100

    cloudinary config

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -4,7 +4,7 @@ const cloudinaryModule = require("cloudinary");
 const cloudinary = cloudinaryModule.v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
