e8f4aa615679f339fc32c6b792e4582c2b3c5974
unknown <kennedyhillary6@gmail.com>
2023-01-16 14:57:58 +0100

    isAdmin

---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const auth = (req, res, next) => {
+  const token = req.headers("X-auth-token");
+  if (!token) return res.status(401).send("Access denied, Not authenticated");
+
+  try {
+    const secretKey = process.env.JWT_SECRET_KEY;
+    const user = jwt.verify(token, secretKey);
+    req.user = user;
+    next();
+  } catch (ex) {
+    return res.status(400).send("Access denied, Invalid auth token");
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  auth(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      return res.status(403).send("Access denied, Not authorized");
+    }
+  });
+};
+module.exports = { isAdmin, auth };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,6 +3,7 @@ const {
   CreateProduct,
   fetchProducts,
 } = require("../controllers/ProductController");
+const { isAdmin } = require("../middlewares/auth");
 const ProductRouter = express.Router();
 const validatorMiddleware = require("../middlewares/validatormiddleware");
 const ProductValidator = require("../validators/productValidator");
@@ -10,6 +11,7 @@ const ProductValidator = require("../validators/productValidator");
 ProductRouter.post(
   "/api/products",
   validatorMiddleware(ProductValidator.Productschema, "body"),
+  isAdmin,
   CreateProduct
 );
 
