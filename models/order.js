5b54507eea51fdca72b431639ba05171aca815a3
unknown <kennedyhillary6@gmail.com>
2023-01-18 08:08:48 +0530

    order refactor

--- models/order.js
+++ models/order.js
@@ -10,17 +10,7 @@ const orderSchema = new mongoose.Schema(
       type: String,
     },
     paymentIntentId: { type: String },
-    products: [
-      {
-        id: { type: String },
-        name: { type: String },
-        brand: { type: String },
-        desc: { type: String },
-        price: { type: String },
-        image: { type: String },
-        cartQuantity: { type: Number },
-      },
-    ],
+    products: [],
     subtotal: { type: Number, required: true },
     total: { type: Number, required: true },
     shipping: { type: Object, required: true },
