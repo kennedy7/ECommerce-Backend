91d90e3831a52825d0676f9a225a7b970e1eca0a
unknown <kennedyhillary6@gmail.com>
2023-02-21 17:01:07 +0100

    refactor

---
+++ controllers/authController.js
@@ -0,0 +1,30 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const genAuthToken = require("../utils/genAuthToken");
+
+exports.LoginUser = async (req, res) => {
+  const { email, password } = req.body;
+  let user = await User.findOne({ email: email });
+  if (!user) return res.status(400).send(" Invalid Email or Password ");
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) return res.status(400).send(" Invalid Email or Password ");
+  const token = genAuthToken(user);
+  res.send(token);
+};
+
+exports.RegisterUser = async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send(" User with this email exist... ");
+
+  const { name, email, password } = req.body;
+
+  user = new User({
+    name: name,
+    email: email,
+    password: password,
+  });
+  user.password = await bcrypt.hash(user.password, 10);
+  user = await user.save();
+  const token = genAuthToken(user);
+  res.send(token);
+};
