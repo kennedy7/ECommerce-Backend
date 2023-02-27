c441e5e93f6d5fe2589e953cf0157ffedcabef7f
unknown <kennedyhillary6@gmail.com>
2023-02-27 09:39:37 +0100

    refactor

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -8,46 +8,10 @@ const {
   getUser,
   updateUser,
 } = require("../controllers/authController");
+const { getUsersMonthlyStats } = require("../controllers/userController");
 
 //get users Stats for the month compare to last month
-usersRouter.get("/api/users/stats", isAdmin, async (req, res) => {
-  const previousMonth = moment()
-    .month(moment().month() - 1)
-    .set("date", 1)
-    .format("YYYY-MM-DD HH-mm-ss");
-
-  try {
-    const users = await User.aggregate([
-      {
-        //starting from previous month i.e >=
-        $match: { createdAt: { $gte: new Date(previousMonth) } },
-      },
-      {
-        $project: {
-          time: {
-            $concat: [
-              { $substr: [{ $year: "$createdAt" }, 0, 4] },
-              " - ",
-              { $substr: [{ $month: "$createdAt" }, 0, 2] },
-            ],
-          },
-        },
-      },
-
-      {
-        $group: {
-          _id: "$time",
-          total: { $sum: 1 },
-        },
-      },
-    ]);
-    res.status(200).send(users);
-    // console.log(users);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-});
+usersRouter.get("/api/users/stats", isAdmin, getUsersMonthlyStats);
 //get all users
 usersRouter.get("/api/users", isAdmin, getAllUsers);
 
