4e7a5fdddb0a35b43b800ac5fce6454c52a09d7f
kennedy7 <kennedyhillary6@gmail.com>
2024-07-08 13:17:25 +0100

    reset password

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { RegisterUser, LoginUser } = require("../controllers/authController");
+const { RegisterUser, LoginUser, ForgotPassword, ResetPassword } = require("../controllers/authController");
 const validatorMiddleware = require("../middlewares/validatormiddleware");
 const AuthValidator = require("../validators/authValidators");
 const router = express.Router();
@@ -13,6 +13,18 @@ router.post(
   "/api/login",
   validatorMiddleware(AuthValidator.Loginschema, "body"),
   LoginUser
+);
+
+router.post(
+  '/api/forgot-password',
+  validatorMiddleware(AuthValidator.ForgotPasswordSchema, 'body'),
+  ForgotPassword
+);
+
+router.post(
+  '/api/reset-password',
+  validatorMiddleware(AuthValidator.ResetPasswordSchema, 'body'),
+  ResetPassword
 );
 
 module.exports = router;
